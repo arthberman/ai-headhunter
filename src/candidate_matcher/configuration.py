8c6035829217e1d7032082624ba3949e68340185
arthberman <arthberman@gmail.com>
2024-09-27 10:31:20 +0200

    update

--- src/candidate_matcher/configuration.py
+++ src/candidate_matcher/configuration.py
@@ -8,7 +8,7 @@ class Configuration(BaseModel):
     """The configuration for the matcher."""
 
     analysis_model: str = Field(
-        default="bedrock_converse/eu.anthropic.claude-3-5-sonnet-20240620-v1:0",
+        default="bedrock_converse/anthropic.claude-3-5-sonnet-20240620-v1:0",
         description="The name of the language model to use for the analysis. "
         "Should be in the form: provider/model-name.",
     )
@@ -20,13 +20,13 @@ class Configuration(BaseModel):
     )
 
     synthesis_model: str = Field(
-        default="bedrock_converse/eu.anthropic.claude-3-5-sonnet-20240620-v1:0",
+        default="bedrock_converse/anthropic.claude-3-5-sonnet-20240620-v1:0",
         description="The name of the language model to use for the synthesis. "
         "Should be in the form: provider/model-name.",
     )
 
     career_path_model: str = Field(
-        default="bedrock_converse/eu.anthropic.claude-3-5-sonnet-20240620-v1:0",
+        default="bedrock_converse/anthropic.claude-3-5-sonnet-20240620-v1:0",
         description="The name of the language model to use for the career path. "
         "Should be in the form: provider/model-name.",
     )
