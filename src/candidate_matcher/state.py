35cf3c46d2ee15de0ed1c60b196d92d17dd0666d
arthberman <arthberman@gmail.com>
2024-09-30 11:51:17 +0200

    update

--- src/candidate_matcher/state.py
+++ src/candidate_matcher/state.py
@@ -28,9 +28,7 @@ class MainGraphState(BaseModel):
 
     education_enrichment: Annotated[List[SchoolInfo], operator.add]
     experience_enrichment: Annotated[List[CompanyInfo], operator.add]
-    language_enrichment: Optional[List[LanguageProficiency]] = Field(
-        default_factory=list
-    )
+    language_enrichment: Annotated[List[LanguageProficiency], operator.add]
 
     scored_criterion: Annotated[List[ScoredCriterion], operator.add]
     career_path: Optional[CareerPathOutput] = Field(default=None)
