ba97cad3c2357cb0612f650f819c139a9bcaccc9
arthberman <arthberman@gmail.com>
2024-10-21 17:18:30 +0200

    update

--- src/scorecard/graph.py
+++ src/scorecard/graph.py
@@ -32,15 +32,15 @@ def compile_scorecard_graph() -> StateGraph:
     )
     workflow.add_node("generate_synthesis", node_synthesis, retry=get_retry_policy())
     workflow.add_node("generate_structure", node_scorecard_structure)
-    workflow.add_node("judge_structure", node_judge_scorecard_structure)
+    # workflow.add_node("judge_structure", node_judge_scorecard_structure)
     # Define the edges
     workflow.add_edge(START, "enrichment")
     workflow.add_edge("enrichment", "generate_job_posting")
     workflow.add_edge("generate_job_posting", "generate_questions")
     workflow.add_edge("generate_questions", "generate_synthesis")
     workflow.add_edge("generate_synthesis", "generate_structure")
-    workflow.add_edge("generate_structure", "judge_structure")
-    workflow.add_edge("judge_structure", "generate_context")
+    # workflow.add_edge("generate_structure", "judge_structure")
+    workflow.add_edge("generate_structure", "generate_context")
     workflow.add_edge("generate_context", "generate_scoring_distribution")
     workflow.add_edge("generate_scoring_distribution", END)
 
