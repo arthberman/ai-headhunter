9bf5a456a0d12f486f4946ba676d757090ecfeb8
arthberman <arthberman@gmail.com>
2025-02-04 18:49:07 +0100

    wip

--- src/feedback_chat/state.py
+++ src/feedback_chat/state.py
@@ -1,4 +1,4 @@
-from typing import Annotated
+from typing import Annotated, List
 
 from langgraph.graph.message import AnyMessage, add_messages
 from pydantic import BaseModel, Field
@@ -8,7 +8,7 @@ class FeedbackChatInputGraphState(BaseModel):
     """State of the feedback chat input."""
 
     profile: str = Field(...)
-    selected_elements: str = Field(...)
+    selected_elements: List[str] = Field(...)
     job_synthesis: str = Field(...)
     messages: Annotated[list[AnyMessage], add_messages] = Field(default_factory=list)
 
