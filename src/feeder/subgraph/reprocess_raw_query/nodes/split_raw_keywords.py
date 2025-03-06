266cde3a7cf0f42d70a12b621ef0db239bbcdf68
sushi057 <suvash2077@gmail.com>
2025-03-06 14:58:22 +0545

    refactor: Replace commands with add_edge
    Improve readability in annotation queues

--- src/feeder/subgraph/reprocess_raw_query/nodes/split_raw_keywords.py
+++ src/feeder/subgraph/reprocess_raw_query/nodes/split_raw_keywords.py
@@ -1,17 +1,10 @@
-from typing import Literal
-
-from langgraph.types import Command
-
 from feeder.subgraph.reprocess_raw_query.state import ReprocessRawQueryInputState
 
 
 def split_raw_keywords(
     state: ReprocessRawQueryInputState,
-) -> Command[Literal["combine_raw_query"]]:
+):
     """Separate raw keywords from json_object."""
     raw_keywords = state.json_object.keywords
 
-    return Command(
-        update={"keywords": raw_keywords},
-        goto="combine_raw_query",
-    )
+    return {"keywords": raw_keywords}
